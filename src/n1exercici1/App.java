9dbbef5ec6b54ebaf7de1dd2cc6540fd826da1d4
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-30 11:59:19 +0200

    n1exercici1 done & video

--- src/n1exercici1/App.java
+++ src/n1exercici1/App.java
@@ -5,12 +5,15 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		NoGenericMethods<String> test = new NoGenericMethods<String>("segundo", "primero", "tercero");
+		NoGenericMethods<String> test = new NoGenericMethods<String>("second", "first", "third");
 		
 		NoGenericMethods<Integer> test2 = new NoGenericMethods<Integer>(3, 2, 1);
 		
 		System.out.println(test2.getO2());
 		
-		System.out.println(test.getO3());
+		System.out.println(test.getO1());
+		
+		test.setO1("First");
+		System.out.println(test.getO1());
 	}
 }
